7af487b9d73b7ff4fd9349e9571888acd7d6ccaa
Anish <anish.mathur@lincode.ai>
2025-04-22 17:01:14 +0530

    Feat : defined folder structure and added basic node structure

---
+++ internals/node.go
@@ -0,0 +1,118 @@
+package internals
+
+const BFactor = 4
+
+type Node struct {
+	// for normal non-leaf /internal node
+	isLeaf bool
+	keys []int
+	children []interface{}
+
+	// for leaf nodes
+	values [][]byte
+	next int
+	prev int
+}
+
+// to create internal node
+func NewInternalNode() *Node {
+	return &Node{
+		isLeaf : false,
+		keys : make([]int, 0, BFactor-1),
+		children : make([]any, 0, BFactor),
+		next : -1,
+		prev : -1,
+	}
+}
+
+// to create leaf node
+func NewLeafNode() *Node {
+	return &Node{
+		isLeaf : true,
+		keys : make([]int, 0, BFactor-1),
+		children : nil,
+		values : make([][]byte, 0, BFactor-1),
+		next : -1,
+		prev : -1,
+	}
+}
+
+// to check if node is full, this needs to be methord
+func (n *Node) IsNodeFull() bool {
+	// leaf nodes are sctrictly checked with keys while internal nodes "fullness" depends on children
+	if n.isLeaf{
+		return len(n.keys)>=BFactor-1
+	}
+	return len(n.children)>=BFactor
+}
+
+// to find index of a key in a node
+func (n *Node) FindAKey(keyToFind int) int {
+	for ind, key := range n.keys {
+		if keyToFind <= key {
+			return ind
+		}
+	}
+	return len(n.keys)
+}
+
+// insertion
+func (n *Node) InsertIntoLeaf(key int, value []byte) {
+	if !n.isLeaf {
+		panic("Invalid method called, insertion in non leaf node called")
+	}
+	idx := n.FindAKey(key)
+	// checking for existing key and updating it
+	if idx < len(n.keys) && n.keys[idx] == key {
+		n.values[idx] = value
+		return
+	}
+
+	// need to create new key and shift array
+	n.keys = append(n.keys, 0)
+	copy(n.keys[idx+1:], n.keys[idx:])
+	n.keys[idx] = key
+	// same for values
+	n.values = append(n.values, nil)
+	copy(n.values[idx+1:], n.values[idx:])
+	n.values[idx]=value
+}
+
+
+// splitting leaf and returns the new node and split key
+func (n *Node) SplitLeaf() (*Node, int) {
+	if !n.isLeaf {
+		panic("Invalid method called, spliting in leaf node called")
+	}
+	// finding middle element and spliting node
+	mid := len(n.keys)/2
+	newNode := NewLeafNode()
+	// ellipsis operator (...) to open the values if slice/list
+	newNode.keys = append(newNode.keys, n.keys[mid:]...)
+	newNode.values = append(newNode.values, n.values[mid:]...)
+	n.keys = n.keys[:mid]
+	n.values = n.values[:mid]
+
+	// updateing ptrs
+	newNode.next = n.next
+	newNode.prev = n.next
+	n.next = newNode.prev
+	return newNode, newNode.keys[0]
+}
+
+// spliting internal node and returns the new node and split key
+// this is similiar to splitleaf, just with children updated instead of values
+func (n *Node) SplitInternal() (*Node, int) {
+	if n.isLeaf {
+		panic("Invalid method called, spliting in non leaf node called")
+	}
+	mid := len(n.keys)/2
+	newNode := NewInternalNode()
+	splitKey := n.keys[mid]
+	newNode.keys = append(newNode.keys, n.keys[mid+1:]...)
+	newNode.children = append(newNode.children, n.children[mid+1:]...)
+	n.keys = n.keys[:mid]
+	n.children = n.children[:mid+1]
+	return newNode, splitKey
+}
+
